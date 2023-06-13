cae6fad4b4e9b19ae9b8f107cd1e47d1c6513d28
maggie kaminska <marg.adm.speak@gmail.com>
2023-06-13 16:57:29 +0100

    add contact page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css";
 import Home from "./routes/Home";
 // import About from "./routes/About";
 import Project from "./routes/Project";
-// import Contact from "./routes/Contact";
+import Contact from "./routes/Contact";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -12,8 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />  */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,14 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-// import HeroImg2 from "../components/HeroImg2";
-// import Form from "../components/Form";
+import HeroImg2 from "../components/HeroImg2";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      {/* <HeroImg2 heading="CONTACT." text="Lets have a chat" />
-      {/* <Form /> */}
+      <HeroImg2 heading="CONTACT" />
       <Footer />
     </div>
   );
